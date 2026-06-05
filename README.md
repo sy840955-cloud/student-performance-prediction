e569d6b78e483ab418902cfe22046e90623e73e8
Sunny Yadav <sy840955@gmail.com>
2026-06-05 10:46:40 +0530

    Initial upload of Student Performance Prediction Project
    Student Performance Prediction System using Random Forest Classifier. Includes dataset, training code, model file and documentation.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Student Performance Prediction System
+
+This project predicts whether a student will Pass or Fail using:
+
+- Study Hours
+- Attendance
+- Previous Scores
+
+Algorithm:
+- Random Forest Classifier
+
+Tools:
+- Python
+- Pandas
+- Scikit-Learn
+- GitHub
---
+++ train_model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import joblib
+
+data = pd.read_csv("data/students.csv")
+
+X = data[["StudyHours", "Attendance", "PreviousScore"]]
+y = data["Result"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy:", accuracy)
+
+joblib.dump(model, "student_model.pkl")
+print("Model Saved Successfully")
